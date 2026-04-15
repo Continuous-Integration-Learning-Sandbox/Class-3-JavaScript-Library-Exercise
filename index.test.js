794fe06e554421cbac173bffb95f101e8106a292
Armand-Veress <armand.veress@student.unitbv.ro>
2026-04-15 16:32:30 +0300

    chore: Add Jest tests

---
+++ index.test.js
@@ -0,0 +1,14 @@
+const { generateStrongPassword, isStrongPassword } = require('./index');
+
+test('should generate a strong password', () => {
+    expect(isStrongPassword(generateStrongPassword(12))).toBe(true);
+});
+
+test('should return true for a strong password', () => {
+    expect(isStrongPassword("Hello World")).toBe(false);
+});
+
+test('should return true for a strong password', () => {
+    const password = "ADGR36Nsva";
+    expect(isStrongPassword("pC%mD8TpCKn2")).toBe(true);
+});
